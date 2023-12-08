4d3b2c4c1922c5966b61aad0bb5523bca4a6d40b
Nwaigba66 <glorianwaigba@gmail.com>
2023-12-08 23:07:56 +0000

    Added Typed Arrays

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,6 @@
+export default function createInt8TypedArray(length, position, value) {
+  if (position >= length) throw new Error('Position outside range');
+  const arrayBuffer = new DataView(new ArrayBuffer(length));
+  arrayBuffer.setInt8(position, value);
+  return arrayBuffer;
+}
